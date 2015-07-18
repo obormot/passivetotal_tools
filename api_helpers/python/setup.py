7791efbcb8ba5bc154293afeb0b8833d04350936
Bhaskar Karambelar <Bhaskar.Karambelar1@verizon.com>
2015-07-18 16:40:16 -0400

    Minimum Stuff for installing as a module

---
+++ api_helpers/python/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='passivetotal',
+      version='1.0',
+      description='PassiveTotal Python API',
+      author='PassiveTotal Team',
+      author_email='brandon@9bplus.com',
+      url='https://www.passivetotal.org/',
+      py_modules=['passivetotal'],
+      )
