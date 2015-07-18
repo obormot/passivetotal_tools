7a29707cd0f741379b607de0b8425f212e678eeb
PassiveTotal <admin@passivetotal.org>
2015-07-18 15:04:33 -0700

    Update setup.py

--- api_helpers/python/setup.py
+++ api_helpers/python/setup.py
@@ -6,7 +6,7 @@ setup(name='passivetotal',
       version='1.0',
       description='PassiveTotal Python API',
       author='PassiveTotal Team',
-      author_email='brandon@9bplus.com',
+      author_email='brandon@passivetotal.org',
       url='https://www.passivetotal.org/',
       py_modules=['passivetotal'],
       )
