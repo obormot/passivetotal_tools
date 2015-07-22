05185e9b0bdf93d073e5f651559ce5ffe920c6e3
Bhaskar Karambelar <Bhaskar.Karambelar1@verizon.com>
2015-07-22 14:28:52 -0400

    python 3

--- api_helpers/python/passivetotal.py
+++ api_helpers/python/passivetotal.py
@@ -38,13 +38,13 @@ class set_check(object):
 class PassiveTotal(object):
 	"""Python helper library built on top of the PassiveTotal (www.passivetotal.org)
 	service API. For updated documentation, head over to www.passivetotal.org/api.
-	"""	
+	"""
 	def __init__(self, api_key):
 		"""Initialize our object with a couple helpers and known-states
 		@param	api_key	string	valid API key for PassiveTotal
 		"""
 		self._api_key = api_key
-		
+
 		self._endpoint = 'https://www.passivetotal.org/api/v1/'
 		self._valid_methods = ['GET', 'POST']
 		self._generic_get_types = ['metadata', 'passive', 'subdomains', \
@@ -54,17 +54,17 @@ class PassiveTotal(object):
 									'ever_compromised', 'watching']
 		self._classifications = ['targeted', 'crime', 'multiple', 'benign']
 		self._logger = logging.getLogger('PassiveTotal')
-		
+
 	@property
 	def endpoint(self):
 		"""Endpoint getter"""
 		return self._endpoint
-		
+
 	@endpoint.setter
 	def endpoint(self, endpoint):
 		"""PassiveTotal offers different versions of the API, so a user
-		could in theory could be using something beyond the latest. 
-		
+		could in theory could be using something beyond the latest.
+
 		@param	endpoint	string	fully qualified URL of the API
 		@returns	bool	whether or not the setting was successful
 		"""
@@ -73,15 +73,15 @@ class PassiveTotal(object):
 		if not endpoint.endswith('/'):
 			endpoint += '/'
 		self._endpoint = endpoint
-		
+
 		return True
-	
+
 	@property
 	def logger(self):
 		"""Logger getter"""
 		return self._logger.getEffectiveLevel()
-		
-	@logger.setter	
+
+	@logger.setter
 	def logger(self, level):
 		"""Simple logger to provide feedback when using the library"""
 		logger = logging.getLogger('PassiveTotal')
@@ -100,13 +100,13 @@ class PassiveTotal(object):
 		shandler.setFormatter(format)
 		logger.addHandler(shandler)
 		return logger
-		
+
 	# Pass-through GET methods
 	def get_metadata(self, query_value):
 		return self._router('GET', 'metadata', query_value)
 	def get_passive(self, query_value):
 		return self._router('GET', 'passive', query_value)
-	def get_subdomains(self, query_value): 
+	def get_subdomains(self, query_value):
 		return self._router('GET', 'subdomains', query_value)
 	def get_unique(self, query_value):
 		return self._router('GET', 'unique', query_value)
@@ -122,7 +122,7 @@ class PassiveTotal(object):
 		return self._router('GET', 'dynamic', query_value)
 	def get_watching(self, query_value):
 		return self._router('GET', 'watching', query_value)
-		
+
 	# Pass-through POST methods
 	@set_check('classification', ['targeted', 'crime', 'multiple', 'benign'])
 	def set_classification(self, query_value, **kwargs):
@@ -132,7 +132,7 @@ class PassiveTotal(object):
 		return self._router('POST', 'sinkhole', query_value, kwargs)
 	@set_check('dynamic', ['true', 'false'])
 	def set_dynamic(self, query_value, **kwargs):
-		return self._router('POST', 'dynamic', query_value, kwargs)	
+		return self._router('POST', 'dynamic', query_value, kwargs)
 	@set_check('ever_compromised', ['true', 'false'])
 	def set_ever_compromised(self, query_value, **kwargs):
 		return self._router('POST', 'ever_compromised', query_value, kwargs)
@@ -147,12 +147,12 @@ class PassiveTotal(object):
 		if 'tag' not in kwargs:
 			raise Exception('Tag is a required field')
 		return self._router('POST', 'user/tag/remove', query_value, kwargs)
-	
-	# Helper methods	
+
+	# Helper methods
 	def _router(self, method, query_type, query_value, kwargs=None):
 		"""Generic routing method to get the data to and from
 		PassiveTotal. All methods should route through this.
-		
+
 		@param	method		string	GET or POST
 		@param	query_type	string	a valid query endpoint
 		@param	query_value	string	item to get data for
@@ -168,6 +168,6 @@ class PassiveTotal(object):
 			params.update(kwargs) # update our dict
 			response = requests.post(call_url, data=params, verify=False)
 		self._logger.debug('Response: %s' % str(response))
-		json_response = json.loads(response.content)
+		json_response = json.loads(response.text)
 		self._logger.debug('Loaded JSON: %s' % json_response)
 		return json_response
